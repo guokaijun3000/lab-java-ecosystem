6a6a055cb4bb6581430db1e61b2b166298c1a5e7
guokaijun3000 <guokaijun3000@126.com>
2022-07-03 10:48:08 +0800

    从spring initializr创建项目

---
+++ src/main/java/com/guokaijun3000/lab_java_ecosystem/LabJavaEcosystemApplication.java
@@ -0,0 +1,13 @@
+package com.guokaijun3000.lab_java_ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJavaEcosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guokaijun3000/lab_java_ecosystem/LabJavaEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guokaijun3000.lab_java_ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJavaEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
