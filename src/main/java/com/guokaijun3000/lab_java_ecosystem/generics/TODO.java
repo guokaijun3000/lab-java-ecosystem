ba4bc5dc113ae216e0839c60d7a3ce601aaf0276
guokaijun3000 <guokaijun3000@126.com>
2022-07-04 21:12:41 +0800

    feat(generic): 新增范型TODO项

---
+++ src/main/java/com/guokaijun3000/lab_java_ecosystem/generics/TODO.java
@@ -0,0 +1,21 @@
+package com.guokaijun3000.lab_java_ecosystem.generics;
+
+/**
+ * @author guokaijun
+ * @date 2022/7/4
+ */
+public class TODO {
+	// TO READ
+	// java.lang.reflect.GenericArrayType
+	// java.lang.reflect.ParameterizedType
+	// java.lang.reflect.TypeVariable
+	// java.lang.reflect.WildcardType
+	// java.lang.reflect.GenericDeclaration
+
+	// java.lang.Class.getGenericInfo
+	// java.lang.Class.getGenericSuperclass
+	// java.lang.Class.getGenericInterfaces
+	// java.lang.reflect.GenericDeclaration
+	// java.lang.Class
+	// java.lang.reflect.Type
+}
